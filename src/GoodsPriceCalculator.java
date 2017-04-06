811f5228f2f51d80d341dada2912790d5ba6b69e
jins040 <jins920409@gmail.com>
2017-04-06 16:48:17 +0900

    Using function

--- src/GoodsPriceCalculator.java
+++ src/GoodsPriceCalculator.java
@@ -20,29 +20,51 @@ public class GoodsPriceCalculator {
         int numberOfCigarette = in.nextInt();
 
         // Process
-        int costOfThis = 4500;
-        int costOfMarlboro = 5200;
-        double totalPriceOfCigarette = 0; // 변수 초기화 곡 해주기
+        int totalPriceOfCigarette = dCcalculator(whichCigarette, age, numberOfCigarette);
+//        int costOfThis = 4500;
+//        int costOfMarlboro = 5200;
+//        double totalPriceOfCigarette = 0; // 변수 초기화 곡 해주기
+//
+//        if (whichCigarette == 1) {
+//            totalPriceOfCigarette = costOfThis;
+//        } else {
+//            totalPriceOfCigarette = costOfMarlboro;
+//        }
+//
+//        if (age < 19) {
+//            System.out.printf("할인된 가격은 %.0f원입니다. (%d세 담배요금 20%% 할인)\n", totalPriceOfCigarette * 0.8, age);
+//            totalPriceOfCigarette = totalPriceOfCigarette * 0.8 * numberOfCigarette;
+//        } else if (age >= 45) {
+//            System.out.printf("추가된 가격은 %.0f원입니다. (%d세 담배요금 30%% 추가)\n", totalPriceOfCigarette * 1.3, age);
+//            totalPriceOfCigarette = totalPriceOfCigarette * 1.3 * numberOfCigarette;
+//        } else {
+//            totalPriceOfCigarette = totalPriceOfCigarette * numberOfCigarette;
+//        }
 
-        if (whichCigarette == 1) {
-            totalPriceOfCigarette = costOfThis;
+        // Output
+        System.out.println("물건 가격은 총 " + totalPriceOfCigarette + "원입니다.");
+
+    }
+
+    public static int dCcalculator(int choice, int age, int quantity) {
+
+        double totalPrice = 0;
+
+        if (choice == 1) {
+            totalPrice = 4500;  // 디스 가격
         } else {
-            totalPriceOfCigarette = costOfMarlboro;
+            totalPrice = 5200;  // 말보로 가격
         }
 
         if (age < 19) {
-            System.out.printf("할인된 가격은 %.0f원입니다. (%d세 담배요금 20%% 할인)\n", totalPriceOfCigarette * 0.8, age);
-            totalPriceOfCigarette = totalPriceOfCigarette * 0.8 * numberOfCigarette;
+            totalPrice = totalPrice * 0.8;
+            System.out.printf("할인된 가격은 %.0f원입니다. (%d세 담배요금 20%% 할인)\n", totalPrice, age);
         } else if (age >= 45) {
-            System.out.printf("추가된 가격은 %.0f원입니다. (%d세 담배요금 30%% 추가)\n", totalPriceOfCigarette * 1.3, age);
-            totalPriceOfCigarette = totalPriceOfCigarette * 1.3 * numberOfCigarette;
-        } else {
-            totalPriceOfCigarette = totalPriceOfCigarette * numberOfCigarette;
+            totalPrice = totalPrice * 1.3;
+            System.out.printf("추가요금은 %.0f원입니다. (%d세 담배요금 30%% 추가)\n", totalPrice, age);
         }
 
-        // Output
-        System.out.println("물건 가격은 총 " + (int)totalPriceOfCigarette + "원입니다.");
-
+        return (int)totalPrice * quantity;
 
     }
 }
